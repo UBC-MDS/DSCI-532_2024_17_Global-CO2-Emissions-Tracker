3c73aa8b6eede95e28045fb8daa0210285f28faf
Jing Wen <wenjing971219@gmail.com>
2024-04-05 09:22:40 -0700

    modified the script for deployment test, changed the file path

--- src/app.py
+++ src/app.py
@@ -4,9 +4,9 @@ import plotly.express as px
 import pandas as pd
 
 # Load the dataset and Preprocessing
-carbon_df = pd.read_csv('../data/proceed/carbon_emission_proceed.csv')
+# carbon_df = pd.read_csv('../data/proceed/carbon_emission_proceed.csv')
 #### CHANGE IT WHEN DEPLOYMENT
-# carbon_df = pd.read_csv('data/proceed/carbon_emission_raw.csv')
+carbon_df = pd.read_csv('data/proceed/carbon_emission_raw.csv')
 
 melted_df = carbon_df.drop(columns=['Country Code']).melt(id_vars=["Country Name", "Region"], var_name="Year", value_name="Emissions")
 melted_df['Year'] = melted_df['Year'].astype(int)  # Ensure 'Year' is an integer for plotting
