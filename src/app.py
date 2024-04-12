13eeb5c6b759d3af5d6b07e34532d66f34d41d8e
Jing Wen <wenjing971219@gmail.com>
2024-04-12 10:37:54 -0700

    modified the script for deployment

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from callbacks import register_callbacks
 ### app layout
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 #### USE IT WHEN DEPLOYMENT
-# server = app.server
+server = app.server
 
 melted_df = load_data()
 app.layout = create_layout(app, melted_df)  
@@ -16,6 +16,6 @@ register_callbacks(app, melted_df)
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='127.0.0.1', port=8050)
+    # app.run_server(debug=True, host='127.0.0.1', port=8050)
     #### CHANGE IT WHEN DEPLOYMENT
-    # app.run_server(debug=False)
+    app.run_server(debug=False)
