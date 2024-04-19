7432908106f75b5c4c1df7b96359aa7519db8f1a
Jing-19 <64814572+Jing-19@users.noreply.github.com>
2024-04-19 10:16:25 -0700

    addressed comments about bar plots, pie plots and color (#71)
    * addressed comments abuot bar plots, pie plots and color
    
    * finished change to map based on the reflection
    
    * update load date with parquet data type
    
    * add parquet file to proceed/
    
    * add place holder for initial line chart
    
    * change .yaml file and requirement.txt file to include flask_catching
    
    * add cache to function
    
    * fixed hocky text on top of map
    
    * modified script for deployment
    
    * added fastparquet and pyarrow
    
    * fixed the title for pie chart
    
    ---------
    
    Co-authored-by: tang yili <yltang92@student.ubc.ca>

--- src/app.py
+++ src/app.py
@@ -1,22 +1,32 @@
 import dash
 import dash_bootstrap_components as dbc
+from flask_caching import Cache 
 
+#### USE IT WHEN DEPLOYMENT
 from src.data import load_data
 from src.components import create_layout
 from src.callbacks import register_callbacks
 
+# from data import load_data
+# from components import create_layout
+# from callbacks import register_callbacks
+
 ### app layout
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'filesystem',  
+    'CACHE_DIR': 'cache-directory'
+})
 #### USE IT WHEN DEPLOYMENT
 server = app.server
 
-melted_df = load_data()
+melted_df = load_data(cache)
 app.layout = create_layout(app, melted_df)  
-register_callbacks(app, melted_df)  
+register_callbacks(app, cache, melted_df) 
 
 
 
 if __name__ == '__main__':
-    # app.run_server(debug=True, host='127.0.0.1', port=8050)
+    app.run_server(debug=True, host='127.0.0.1', port=8050)
     #### CHANGE IT WHEN DEPLOYMENT
     app.run_server(debug=False)
--- src/callbacks.py
+++ src/callbacks.py
@@ -1,55 +1,110 @@
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
+import dash
+from dash.exceptions import PreventUpdate
 
-def register_callbacks(app, melted_df):
-    ### define function for plot 
+
+def register_callbacks(app, cache, melted_df):
+    @cache.memoize(timeout=300) 
+    def get_filtered_data(melted_df, selected_countries, selected_years):
+        return melted_df[
+            (melted_df['Country Name'].isin(selected_countries)) &
+            (melted_df['Year'] >= selected_years[0]) &
+            (melted_df['Year'] <= selected_years[1])
+        ]
     @app.callback(
-        Output('emissions-time-series', 'figure'),
-        [Input('country-dropdown', 'value'), Input('year-slider', 'value')]
+        [Output('country-dropdown', 'value'),
+         Output('emissions-time-series', 'figure')],
+        [Input('emissions-map-chart', 'clickData'),
+         Input('country-dropdown', 'value'),
+         Input('year-slider', 'value')]
     )
+    def line_from_map_click(clickData, selected_countries, selected_years):
+        ctx = dash.callback_context
+        if not ctx.triggered:
+            placeholder_figure = {
+                'data': [],
+                'layout': {
+                    'xaxis': {'visible': False},
+                    'yaxis': {'visible': False},
+                    'annotations': [{
+                        'text': 'Please select a country to visualize the carbon emission line chart',
+                        'xref': 'paper',
+                        'yref': 'paper',
+                        'showarrow': False,
+                        'font': {
+                            'size': 16
+                        },
+                        'align': 'center'
+                    }]
+                }
+            }
+            return [selected_countries, placeholder_figure]
+        
+        input_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    def update_graph(selected_countries, selected_years):
-        if not selected_countries or not selected_years:
-            return px.line(title='Select countries and year range to see CO2 Emissions Over Time')
-        
-        df_filtered = melted_df[(melted_df['Country Name'].isin(selected_countries)) & 
-                                (melted_df['Year'] >= selected_years[0]) & 
-                                (melted_df['Year'] <= selected_years[1])]
-        
-        fig = px.line(df_filtered, x='Year', y='Emissions', color='Country Name',
-                    title='CO2 Emissions Over Time for Selected Countries')
-        
-        return fig
+        if selected_countries is None:
+            selected_countries = []
+
+        if input_id == 'emissions-map-chart' and clickData:
+            country_name = clickData['points'][0]['location']
+            if country_name not in selected_countries:
+                selected_countries.append(country_name)
+            else:
+                selected_countries.remove(country_name)
+
+        # Proceed with filtering data and creating the line chart
+        df_filtered = get_filtered_data(melted_df, selected_countries, selected_years)
+
+        line_fig = px.line(df_filtered, x='Year', y='Emissions', color='Country Name',
+                           title='CO2 Emissions Over Time for Selected Countries')
+        line_fig.update_yaxes(title_text='Emissions (kt CO2)')
+        return [selected_countries, line_fig]
 
 
     ## Bar Chart @ Jing
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+
+    def generate_color_map(df_top_countries):
+        color_discrete_map = {country: colors[i] for i, country in enumerate(df_top_countries['Country Name'])}
+        return color_discrete_map
+    
     @app.callback(
         Output('emissions-bar-chart', 'figure'),
         [Input('region-dropdown', 'value')]
     )
-
+    
     def update_bar_chart(selected_regions):
         if not selected_regions:
             return px.bar(title='Choose any Region(s): <br>For CO2 Emissions Bar Chart')
 
         df_filtered_by_region = melted_df[melted_df['Region'].isin(selected_regions)]
-        df_top_countries = df_filtered_by_region.groupby('Country Name').agg({'Emissions':'sum'}).nlargest(5, 'Emissions').reset_index()
+        df_countries = df_filtered_by_region.groupby('Country Name', as_index=False).agg({'Emissions':'sum'})
+        df_top_countries = df_countries.sort_values('Emissions', ascending=False).head(5)
+        color_discrete_map = generate_color_map(df_top_countries)
 
         max_emissions_value = df_top_countries['Emissions'].max()
         y_axis_max = max_emissions_value * 1.2
 
         fig = px.bar(df_top_countries, x='Country Name', y='Emissions', text='Emissions',
-                    title='Top 5 Countries\' Total CO2 Emissions<br>in Selected Region(s)')
-
+                 title='Top 5 Countries\' Cumulative CO2 Emissions<br>in Selected Region(s)',
+                 color='Country Name',
+                 color_discrete_map=color_discrete_map)
+        
         fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
         fig.update_xaxes(title_text='')
-        fig.update_yaxes(range=[0, y_axis_max])
+        fig.update_yaxes(title_text='Emissions (kt CO2)', range=[0, y_axis_max])
 
         return fig
 
 
     # Pie Chart @ Jo
+    def generate_color_sequence(df_top_countries):
+        colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#7f7f7f']  
+        color_sequence = [colors[i] for i in range(len(df_top_countries) + (1 if 'Others' in df_top_countries['Country Name'].values else 0))]
+        return color_sequence
+
     @app.callback(
         Output('emissions-pie-chart', 'figure'),
         [Input('region-dropdown', 'value')]
@@ -69,39 +124,37 @@ def register_callbacks(app, melted_df):
         else:
             df_display = df_top_countries
         
+        color_sequence = generate_color_sequence(df_top_countries)
         fig = px.pie(data_frame=df_display,
                     names='Country Name', 
                     values='Emissions',
                     hover_data={'Country Name': True, 'Emissions': ':.2f'}, 
                     hole=.2,
-                    title='CO2 Emissions of the Selected Region(s): <br>Top 5 Countries and Others')
+                    title='Cumulative CO2 Emission: <br>Top 5 Countries and Others')
         
-        fig.update_traces(hovertemplate='%{label}: <br>Percentage: %{percent} <br>CO2 Emission: %{value} MT/capita<br>', textposition='outside')
-
+        fig.update_traces(hovertemplate='%{label}: <br>Percentage: %{percent} <br>CO2 Emission: %{value} MT/capita<br>', textposition='outside',
+                      marker=dict(colors=color_sequence))
         return fig
 
-    ## Map Chart @ Yili
+    # Map Chart @ Yili
     @app.callback(
         Output('emissions-map-chart', 'figure'),
-        [Input('country-dropdown', 'value'), Input('year-slider', 'value'), Input('data-scope-check', 'value')]
+        [Input('country-dropdown', 'value'), Input('year-slider', 'value')]
     )
 
-    def update_map(selected_countries, selected_years, scope):
-        if 'ALL' in scope:
-            df_filtered = melted_df[
-                (melted_df['Year'] >= selected_years[0]) &
-                (melted_df['Year'] <= selected_years[1])
-            ]
-        elif not selected_countries or not selected_years:
-            
-            return px.choropleth(title="Select countries and year range to see the map")
-        else:
+    def update_map(selected_countries, selected_years):
+        if selected_countries:
             df_filtered = melted_df[
                 (melted_df['Country Name'].isin(selected_countries)) &
                 (melted_df['Year'] >= selected_years[0]) &
                 (melted_df['Year'] <= selected_years[1])
             ]
-    
+        else:
+            df_filtered = melted_df[
+                (melted_df['Year'] >= selected_years[0]) &
+                (melted_df['Year'] <= selected_years[1])
+            ]
+        
         # Calculate summary statistics for each country
         stats_by_country = df_filtered.groupby('Country Name').agg(
             Total_Emissions=pd.NamedAgg(column='Emissions', aggfunc='sum'),
@@ -125,12 +178,14 @@ def register_callbacks(app, melted_df):
                 'Max_Emissions': ':.2f',
                 'Min_Emissions': ':.2f'
             },
-            color_continuous_scale=px.colors.qualitative.Dark24,
-            title="CO2 Emissions by Country"
+            color_continuous_scale=px.colors.sequential.Teal,
+            # title="CO2 Emissions by Country",
+            labels={'Total_Emissions': 'Total Emissions (kt CO2)'}
         )
 
-        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, clickmode='event+select' )
         fig.update_geos(projection_type="natural earth")
 
         return fig
 
+    
